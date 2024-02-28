c67a409680dda4efbcde2c479385743ea3824a8e
Awais-Mazher <151544882+Awais-Mazher@users.noreply.github.com>
2024-02-27 22:13:13 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+let nameError = document.getElementById("name-error");
+let emailError = document.getElementById("email-error");
+let messageError = document.getElementById("message-error");
+let submitError = document.getElementById("submit-error");
+let contactBtn = document.getElementById("contact-btn");
+let contactBox = document.getElementById("contact-box");
+let crossIcon = document.getElementById("cross-icon");
+let mainContent = document.getElementById("main-content");
+
+function validateName(){
+    let name = document.getElementById("contact-name").value;
+
+    if(name.length == 0){
+        nameError.innerHTML = 'Name is required';
+        return false;
+    }
+
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = 'Write full name';
+        return false;
+    }
+
+    nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validateEmail(){
+    let email = document.getElementById("contact-email").value;
+
+    if(email.length == 0){
+        emailError.innerHTML = 'Email is required';
+        return 0;
+    }
+
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailError.innerHTML = 'Email Invalid';
+        return false;
+    }
+
+    emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validateMessage(){
+    let message = document.getElementById("contact-message").value;
+    let required = 30;
+    let left = required - message.length;
+
+    if(left>0){
+        messageError.innerHTML = left + ' more characters required';
+        return false;
+    }
+
+    messageError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validateForm(){
+    if(!validateName()  || !validateEmail()  || !validateMessage()){
+        submitError.style.display = 'block';
+        submitError.innerHTML = 'Please fix error to submit';
+        setTimeout(function(){submitError.style.display = 'none'} , 3000);
+        return false;
+    }
+}
+
+contactBtn.addEventListener("click" , ()=>{
+    contactBox.classList.add("contact-open");
+})
+
+crossIcon.addEventListener("click" , ()=>{
+    contactBox.classList.remove("contact-open");
+})
+
+
